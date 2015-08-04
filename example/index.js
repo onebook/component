1d2512d18984f57c289d3a729139c4494376762a
haoxin <coderhaoxin@outlook.com>
2015-08-04 10:36:52 +0800

    mv progress, slide, popup, mask to scssbase, rm hover

--- example/index.js
+++ example/index.js
@@ -8,55 +8,9 @@ import { dialog } from '../dialog/dialog'
 window.dialogSlide = dialogSlide
 window.dialog = dialog
 window.upload = upload
-window.pop = pop
-
-slide1()
-slide2()
-progress()
 
 function $(selector) {
   return document.querySelector(selector)
-}
-
-function pop(type) {
-  var node = $('.com-popup')
-  node.classList.remove('fade')
-  node.classList.remove('scale')
-  setTimeout(() => {
-    node.classList.add(type)
-  }, 10)
-}
-
-function progress() {
-  $('.percent').style.width = '30%'
-  setTimeout(function() {
-    $('.percent').style.width = '70%'
-    setTimeout(function() {
-      $('.percent').style.width = '100%'
-    }, 2000)
-  }, 2000)
-}
-
-function slide1() {
-  $('#slide1 .content').style.left = '0'
-  $('#slide1 .content').style.opacity = ''
-  setTimeout(() => {
-    $('#slide1 .content').style.left = '80px'
-    $('#slide1 .content').style.opacity = '1'
-    setTimeout(() => {
-      $('#slide1 .content').style.left = '160px'
-      $('#slide1 .content').style.opacity = '0'
-    }, 800)
-  }, 800)
-}
-
-function slide2() {
-  setTimeout(() => {
-    $('#slide2').style.height = '150px'
-    setTimeout(() => {
-      $('#slide2').style.height = '0'
-    }, 1000)
-  }, 1000)
 }
 
 (() => {
